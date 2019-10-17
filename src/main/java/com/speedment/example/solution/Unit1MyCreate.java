0ace2f2bd0b633050c8ba0d9f2d01afe19f12271
julgus <gustafsson.julia@live.se>
2019-10-17 09:01:03 +0200

    solved first two units

--- src/main/java/com/speedment/example/solution/Unit1MyCreate.java
+++ src/main/java/com/speedment/example/solution/Unit1MyCreate.java
@@ -3,6 +3,9 @@ package com.speedment.example.solution;
 import com.speedment.example.unit.Unit1Create;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Random;
 import java.util.stream.IntStream;
@@ -12,41 +15,41 @@ public final class Unit1MyCreate implements Unit1Create {
 
     @Override
     public Stream<String> newStreamOfAToC() {
-        return Stream.empty();
+        return Stream.of("B", "C", "A").sorted();
     }
 
     @Override
     public IntStream newIntStreamOfOneToSeven() {
-        return IntStream.empty();
+        return IntStream.rangeClosed(1, 7);
     }
 
     @Override
     public Stream<String> from(String[] array) {
-        return Stream.empty();
+        return Arrays.stream(array);
     }
 
     @Override
     public Stream<String> from(Collection<String> collection) {
-        return Stream.empty();
+        return collection.stream();
     }
 
     @Override
     public IntStream from(String s) {
-        return IntStream.empty();
+        return s.chars();
     }
 
     @Override
     public IntStream infiniteAlternating() {
-        return IntStream.empty();
+        return IntStream.iterate(1, i -> i * (-1));
     }
 
     @Override
     public IntStream infiniteRandomInts(Random rnd) {
-        return IntStream.empty();
+        return rnd.ints();
     }
 
     @Override
     public Stream<String> linesFromPoemTxtFile() throws IOException {
-        return Stream.empty();
+        return Files.lines(Paths.get(FILE_NAME));
     }
 }
--- src/main/java/com/speedment/example/solution/Unit2MyIntermediate.java
+++ src/main/java/com/speedment/example/solution/Unit2MyIntermediate.java
@@ -9,31 +9,31 @@ public final class Unit2MyIntermediate implements Unit2Intermediate {
 
     @Override
     public Stream<String> wordsLongerThanThreeChars(Stream<String> stream) {
-        return Stream.empty();
+        return stream.filter(w -> w.length() > 3);
     }
 
     @Override
     public Stream<String> firstTwoWordsLongerThanThreeChars(Stream<String> stream) {
-        return Stream.empty();
+        return wordsLongerThanThreeChars(stream).limit(2);
     }
 
     @Override
     public Stream<String> firstDistinctTwoWordsLongerThanThreeCharsInAlphabeticOrder(Stream<String> stream) {
-        return Stream.empty();
+        return wordsLongerThanThreeChars(stream).distinct().limit(2).sorted();
     }
 
     @Override
     public IntStream lengthOfWords(Stream<String> stream) {
-        return IntStream.empty();
+        return stream.mapToInt(String::length);
     }
 
     @Override
     public IntStream increasingSawtooth() {
-        return IntStream.empty();
+        return IntStream.range(0, Integer.MAX_VALUE).flatMap(i -> IntStream.range(0, i));
     }
 
     @Override
     public Stream<String> strings(Stream<Object> stream) {
-        return Stream.empty();
+        return stream.filter(o -> (o instanceof String)).map(o -> (String) o);
     }
 }
