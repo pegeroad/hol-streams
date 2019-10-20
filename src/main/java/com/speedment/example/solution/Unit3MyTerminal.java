3cc53c734c8fb4a0c6c81981aa7886ddacfdba05
Gergo Petrik <petrikgergo@gmail.com>
2019-10-20 15:10:43 +0100

    unit3 is solved

--- src/main/java/com/speedment/example/solution/Unit3MyTerminal.java
+++ src/main/java/com/speedment/example/solution/Unit3MyTerminal.java
@@ -3,75 +3,78 @@ package com.speedment.example.solution;
 import com.speedment.example.unit.Unit3Terminal;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static java.util.Collections.*;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 
 public final class Unit3MyTerminal implements Unit3Terminal {
 
     @Override
     public void addToSet(Stream<String> stream, Set<String> set) {
-
+        stream.forEach(set::add);
     }
 
     @Override
     public void addToListInOrder(Stream<String> stream, List<String> list) {
-
+        stream.forEachOrdered(list::add);
     }
 
     @Override
     public Set<String> collectToSet(Stream<String> stream) {
-        return emptySet();
+        return stream.collect(Collectors.toSet());
     }
 
     @Override
     public List<String> collectToList(Stream<String> stream) {
-        return emptyList();
+        return stream.collect(Collectors.toList());
     }
 
     @Override
     public String collectJoining(Stream<String> stream) {
-        return null;
+        return stream.collect(Collectors.joining("->"));
     }
 
     @Override
     public String[] toArray(Stream<String> stream) {
-        return new String[0];
+        return stream.toArray(String[]::new);
     }
 
     @Override
     public Map<String, Integer> collectToMap(Stream<String> stream) {
-        return emptyMap();
+        return stream.collect(Collectors.toMap(Function.identity(), String::length));
     }
 
     @Override
     public int countCharacters(Stream<String> stream) {
-        return 0;
+        return (int) stream.flatMap(s -> s.chars().mapToObj(i -> (char) i)).count();
     }
 
     @Override
     public int maxWordLen(Stream<String> stream) {
-        return 0;
+        return stream.map(String::length).max(Integer::compareTo).orElse(0);
     }
 
     @Override
     public IntSummaryStatistics statistics(Stream<String> stream) {
-        return new IntSummaryStatistics();
+        return stream.collect(Collectors.summarizingInt(String::length));
     }
 
     @Override
     public Optional<String> findLongestString(Stream<String> stream) {
-        return Optional.empty();
+        return stream.max(Comparator.comparingInt(String::length));
     }
 
     @Override
     public Map<Integer, List<String>> wordsGroupedByLength(Stream<String> stream) {
-        return emptyMap();
+        return stream.collect(groupingBy(String::length));
     }
 
     @Override
     public Map<Integer, Long> wordsGroupedByLengthCounted(Stream<String> stream) {
-        return emptyMap();
+        return stream.collect(groupingBy(String::length, counting()));
     }
 
 }
